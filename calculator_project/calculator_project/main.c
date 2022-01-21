af4b4326649414d332c1ff2b74f9655699088213
HyunSwim <coldsz@naver.com>
2022-01-21 20:56:48 +0900

    C파일 제작 완료

--- calculator_project/calculator_project/main.c
+++ calculator_project/calculator_project/main.c
@@ -1,5 +1,119 @@
 #include <stdio.h>
 
+// 덧셈, 뺄셈, 곱셈, 나눗셈, 입력, 출력
+
+int a, b;
+char c;
+
+// input 종범 8번째 줄
+void input() {
+
+}
+
+
+
+
+
+
+
+
+// 더하기 윤선 20번째 줄
+int plus() {
+
+}
+
+
+
+
+
+
+
+
+// 빼기 상호 32번째 줄
+int minus() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// 곱하기 종학 48번째 줄
+int multiple() {
+
+}
+
+
+
+
+
+
+
+
+
+
+// 나누기 지민 62번째
+int dis() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 창기 81번째 줄
+void print() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// main 100번째 줄
 int main() {
 
+	input(); // a, b, c : +, -, *, /
+	int answer;
+	if (c == "+") {
+		answer = plus();
+	}
+	else if (c == '-') {
+		answer = minus();
+	}
+	else if (c == '*') {
+		answer = multiple();
+	}
+	else if (c == '/') {
+		answer = dis();
+	}
+
+	print();
 }
